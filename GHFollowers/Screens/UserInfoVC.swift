8456946ec7d7490d8be1f8d6721277fadbd6c722
Marcel Mravec <marcel.mravec@gmail.com>
2024-01-22 19:37:14 +0100

    add userinfo headerview

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
+    let headerView = UIView()
     var username: String!
     
     override func viewDidLoad() {
@@ -16,15 +17,38 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        
+        layoutUI()
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self else { return }
             switch result {
-            case .success(let user): print(user)
+            case .success(let user):
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
+                
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
     
     @objc func dismissVC() {
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Marcel Mravec on 22.01.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+    
+}
+
+
+struct Constants {
+    
+}
